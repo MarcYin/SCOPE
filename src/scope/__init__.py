87ea49947b262fbba61785aef75d9b91d8551775
MarcYin <marc.yin@example.com>
2026-05-11 15:54:26 +0100

    Bump version to 0.4.5

--- src/scope/__init__.py
+++ src/scope/__init__.py
@@ -180,7 +180,7 @@ __all__ = [
 try:  # pragma: no cover
     __version__ = _pkg_version("SCOPE-RTM")
 except Exception:  # pragma: no cover
-    __version__ = "0.2.0"
+    __version__ = "0.4.5"
 
 
 apply_registry_docstrings(
